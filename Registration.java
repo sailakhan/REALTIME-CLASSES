04eaa2e845585a495a6fb95965284ff5e4fc5856
sailakhan <sailakhan.lakhan36@gmail.com>
2017-07-20 23:59:55 -0500

    Add files via upload

---
+++ Registration.java
@@ -0,0 +1,64 @@
+package jdbc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Registration
+ */
+@WebServlet("/Registration")
+public class Registration extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Registration() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			String name=request.getParameter("user");
+			String password=request.getParameter("password");
+			String sql="insert into LOGIN values(?,?)";
+			
+			Class.forName("org.h2.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test","sa","");
+			
+			PreparedStatement ps=conn.prepareStatement(sql);
+			ps.setString(1, name);
+			ps.setString(2, password);
+			ps.executeUpdate();
+			PrintWriter out=response.getWriter();
+			out.println("You have Registered");
+			
+			
+		}
+		catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ registration.jsp
@@ -0,0 +1,31 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Registration</title>
+</head>
+<body>
+<h3>Welcome</h3>
+<form action="Registration" method="post">
+<table>
+<tr>
+<td>
+<strong>Username</strong></td>
+<td><input type="text" placeholder="Enter your Username" name="user"></td>
+</tr>
+<tr>
+<td>
+<strong>Password </strong></td>
+<td>
+<input type="password" placeholder="Enter your Password" name="password"></td>
+</tr>
+<tr><td></td>
+<td><input type="submit" value="Register"></td>
+</tr>
+</table>
+</form>
+<br>
+</body>
+</html>
